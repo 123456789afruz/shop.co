13d3a53ca244ae8eb7172e3f0a9d999f45b09ba2
123456789afruz <afruzoxunov@gmail.com>
2024-11-26 18:59:56 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import React from 'react'
 import './App.scss'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Shop from './pages/Shop'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 
 const App = () => {
   return (
     <>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/shop' element={<Shop/>}/>
       </Routes>
+      <Footer/>
       
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import Header from '../components/Header/Header'
 import Hero from '../components/Header/Hero/Hero'
 import Brands from '../components/Header/Brands/Brands'
 import Category from '../components/Header/Category/Category'
-import By from '../components/By/By'
+import Browse from '../components/Browse/Browse'
 
 const Home = () => {
   return (
     <>
-      <Header/>
       <Hero/>
       <Brands/>
       <Category title='New arrivals' num="0,4"/>  
       <Category title='Top selling' num="4,8"/>
-      <By title='BROWSE BY dress STYLE'/>
+      <Browse/>
     </>
   )
 }
